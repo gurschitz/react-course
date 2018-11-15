8f4306eaabf6bb2f575ce10a2861b91effb218b1
Gerald Urschitz <gerald.urschitz@gmail.com>
2018-11-15 15:32:39 +0100

    Kurse -> Workshops, remove .io from title

--- web/src/App.js
+++ web/src/App.js
@@ -15,10 +15,10 @@ class App extends Component {
         <Title.Base>
           <Title.H1>
             <span style={{ color: "#96d7c8" }}>{"<"}</span>
-            {"UseReact.io"}
+            {"UseReact"}
             <span style={{ color: "#96d7c8" }}>{"/>"}</span>
           </Title.H1>
-          <Title.Subtitle>React Kurse in der Werkstatt A</Title.Subtitle>
+          <Title.Subtitle>React Workshops in der Werkstatt A</Title.Subtitle>
         </Title.Base>
 
         <div className="text-center mt2">Nächster Termin: </div>
